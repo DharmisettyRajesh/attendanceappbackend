3dfbdeddc8a3fdb3b8282b76f1de55417e47bfe4
unknown <dharmisettyrajesh>
2020-07-16 10:12:01 +0530

    this is awesome

---
+++ public/precache-manifest.1ca631fa50943a1e3994299da3ae1e00.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0146a1cf762e072f4057fe6b45c0b69",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1dc59ff0d4f21f3e3edd",
+    "url": "/static/css/main.1fdda10d.chunk.css"
+  },
+  {
+    "revision": "86f8dc05774eeb0032ea",
+    "url": "/static/js/2.6d6df18c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.6d6df18c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1dc59ff0d4f21f3e3edd",
+    "url": "/static/js/main.5f52e08e.chunk.js"
+  },
+  {
+    "revision": "976c72d3cbc764c2bdab",
+    "url": "/static/js/runtime-main.b7d756a1.js"
+  }
+]);
